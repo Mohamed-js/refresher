9695ce72f933238b77f8286c9292e84cec6e3bf0
Mohamed Atef <atefhamosa@gmail.com>
2023-03-05 01:03:04 +0200

    Update apps_controller.rb

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,13 +2,13 @@ class AppsController < ApplicationController
     require 'net/http'
     def refresh
         while true
-            Net::HTTP.get(URI('https://sellex.onrender.com/refresher'))
+            Net::HTTP.get(URI('https://sellex-store.onrender.com/refresher'))
             sleep 20
             Net::HTTP.get(URI('https://workout-49bf.onrender.com/refresher'))
             sleep 20
-            Net::HTTP.get(URI('https://socket-og0b.onrender.com/asd'))
+            Net::HTTP.get(URI('https://zippy-bronzed-shingle.glitch.me/asd'))
             sleep 20
-            Net::HTTP.get(URI('https://computech.onrender.com'))
+            Net::HTTP.get(URI('https://computek.onrender.com'))
             sleep 20
         end
         render json: "Done....!"
